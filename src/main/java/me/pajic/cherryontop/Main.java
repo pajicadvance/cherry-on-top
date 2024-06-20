bca501acd7f23b5fb5c65a4995d81c3e9d848d53
pajicadvance <pavle.pajic23@gmail.com>
2024-06-20 21:14:52 +0200

    Prepare dev environment

---
+++ src/main/java/me/pajic/cherryontop/Main.java
@@ -0,0 +1,10 @@
+package me.pajic.cherryontop;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
