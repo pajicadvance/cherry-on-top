fbadb69844738907238b07ae40d3f45856028651
pajicadvance <pavle.pajic23@gmail.com>
2024-06-29 18:27:31 +0200

    Add mod config as a global variable to JsonPatcher

---
+++ src/main/java/me/pajic/cherryontop/mixin/jsonpatcher/PatcherMixin.java
@@ -0,0 +1,34 @@
+package me.pajic.cherryontop.mixin.jsonpatcher;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.llamalad7.mixinextras.injector.ModifyReceiver;
+import io.github.mattidragon.jsonpatcher.lang.runtime.EvaluationContext;
+import io.github.mattidragon.jsonpatcher.misc.GsonConverter;
+import io.github.mattidragon.jsonpatcher.patch.Patcher;
+import net.fabricmc.loader.api.FabricLoader;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+@Mixin(value = Patcher.class, remap = false)
+public class PatcherMixin {
+
+    @Shadow @Final private static Gson GSON;
+
+    @ModifyReceiver(
+            method = "buildContext",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lio/github/mattidragon/jsonpatcher/lang/runtime/EvaluationContext$Builder;build()Lio/github/mattidragon/jsonpatcher/lang/runtime/EvaluationContext;"
+            )
+    )
+    private static EvaluationContext.Builder addModConfigAsGlobalVariable(EvaluationContext.Builder instance) throws FileNotFoundException {
+        return instance.variable("cherryontop_config",
+                GsonConverter.fromGson(GSON.fromJson(new FileReader(FabricLoader.getInstance().getConfigDir().resolve("cherry-on-top.json5").toFile()), JsonElement.class)));
+    }
+}
