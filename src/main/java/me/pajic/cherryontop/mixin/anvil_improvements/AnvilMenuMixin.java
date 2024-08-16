a4256fe3264fdecf540fee3b478d0d7905200ac5
pajicadvance <pavle.pajic23@gmail.com>
2024-08-17 00:56:48 +0200

    Fix conflicts with Tax Free Levels

--- src/main/java/me/pajic/cherryontop/mixin/anvil_improvements/AnvilMenuMixin.java
+++ src/main/java/me/pajic/cherryontop/mixin/anvil_improvements/AnvilMenuMixin.java
@@ -1,25 +1,30 @@
 package me.pajic.cherryontop.mixin.anvil_improvements;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.moulberry.mixinconstraints.annotations.IfModLoaded;
 import me.pajic.cherryontop.Main;
 import me.pajic.cherryontop.util.CoTUtil;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AnvilMenu;
+import net.minecraft.world.inventory.DataSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AnvilMenu.class)
+@Mixin(value = AnvilMenu.class, priority = 2000)
 public class AnvilMenuMixin {
 
     @Shadow private int repairItemCountCost;
+
+    @Shadow @Final private DataSlot cost;
 
     @ModifyArg(
             method = "createResult",
@@ -51,17 +56,18 @@ public class AnvilMenuMixin {
         return original;
     }
 
-    @WrapWithCondition(
+    @Inject(
             method = "onTake",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/player/Player;giveExperienceLevels(I)V"
-            )
+            at = @At("HEAD")
     )
-    private boolean noXPCostIfUnenchanted(Player instance, int levels, @Local(argsOnly = true) ItemStack itemStack) {
-        return !Main.CONFIG.anvilImprovements.freeUnenchantedRepairs() ||
-                itemStack.isEnchanted() ||
-                !itemStack.getOrDefault(DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY).isEmpty();
+    private void noXPCostIfUnenchanted(Player player, ItemStack itemStack, CallbackInfo ci) {
+        if (
+                Main.CONFIG.anvilImprovements.freeUnenchantedRepairs() &&
+                !itemStack.isEnchanted() &&
+                itemStack.getOrDefault(DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY).isEmpty()
+        ) {
+            cost.set(0);
+        }
     }
 
     @ModifyArg(
@@ -86,6 +92,22 @@ public class AnvilMenuMixin {
             value -= l;
         }
         return value;
+    }
+
+    @IfModLoaded("taxfreelevels")
+    @Inject(
+            method = "createResult",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z",
+                    ordinal = 2
+            )
+    )
+    private void interceptRenameCostSet(CallbackInfo ci, @Local(ordinal = 0) int i, @Local(ordinal = 1) int j) {
+        // tax free levels forcibly sets the rename cost to 1, this injects after it to revert the cost
+        if (Main.CONFIG.anvilImprovements.freeRenames() && j > 0 && j == i) {
+            cost.set(0);
+        }
     }
 
     @ModifyExpressionValue(
